e5df9ccf38c1059a27b3b7e88d1188f5b123f4e5
99002775 <72464285+99002775@users.noreply.github.com>
2020-10-07 02:20:25 +0530

    Create Heading.c

---
+++ Heading.c
@@ -0,0 +1,10 @@
+char heading()//HEADING
+{
+    system("cls");
+
+	printf("\n\t\t  PSG INSTITUTE OF TECHNOLOGY\n");
+	printf("------------------------------------------------------------------------------------\n\n");
+	printf("\t\t  WELCOME TO THE PSG STUDENT UNION ELECTION\n\n");
+	printf("\t\t\t   **************************\n");
+	printf("\n\n\n");
+}
